795ec41ccfc0b601046343c6674ed4928ee89bd4
Tristone1217 <Tristone1217@163.com>
2021-05-30 11:22:45 +0800

    add the linux command study

---
+++ linux_study/file1.sv
@@ -0,0 +1,4 @@
+this is a example
+this is a example for linux command study
+this is a example for my linux command study
+example example  example example example example
---
+++ linux_study/newfile.sv
@@ -0,0 +1,4 @@
+this is a useful example
+this is a useful example for linux command study
+this is a useful example for my linux command study
+useful example useful example  useful example useful example useful example useful example
