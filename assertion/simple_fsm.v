d72efad4f02794d943d89bc3d34dc89822e23eff
Tristone1217 <Tristone1217@163.com>
2021-06-06 22:16:46 +0800

    add the example of assertion

---
+++ assertion/simple_fsm.v
@@ -0,0 +1,75 @@
+`timescale	1ns/1ns
+///////////////////////////////////////////////////////////////////////////
+// Author		 : 相量子
+// Create Date	 : 2019/05/12
+// Module Name	 : simple_fsm
+// Project Name	 : simple_fsm
+// Target Devices: Altera EP4CE10F17C8
+// Tool Versions : Quartus 13.0
+// Description	 :
+// Revision      : V1.0
+// Additional Comments:
+//////////////////////////////////////////////////////////////////////////
+
+module	simple_fsm(
+	input	wire	sys_clk		,	//系统时钟50MHz
+	input	wire	sys_rst_n	,	//全局复位
+	input	wire	pi_money	,	//投币方式可以为：不投币（0）、投1元（1）
+		
+	output	reg		po_cola			//po_cola为1时出可乐，po_cola为0时不出可乐	
+);
+
+//只有三种状态，使用独热码
+parameter	IDLE = 3'b001;
+parameter	ONE  = 3'b010;
+parameter	TWO  = 3'b100;
+
+reg		[2:0]	state;				
+
+//第一段状态机，描述当前状态state如何根据输入跳转到下一状态
+always@(posedge sys_clk or negedge sys_rst_n)
+	if(sys_rst_n == 1'b0)
+		state <= IDLE;								//任何情况下只要按复位就回到初始状态
+	else	case(state)
+				IDLE	:	if(pi_money == 1'b1)	//判断输入情况
+								state <= ONE;
+							else
+								state <= IDLE;
+								
+				ONE		:	if(pi_money == 1'b1)
+								state <= TWO;							
+							else
+								state <= ONE;
+								
+				TWO		:	if(pi_money == 1'b1)
+								state <= IDLE;
+							else	
+								state <= TWO;
+								
+				default	:		state <= IDLE;		//如果状态机跳转到编码的状态之外也回到初始状态
+			endcase
+
+//第二段状态机，描述当前状态state和输入pi_money如何影响po_cola输出
+always@(posedge sys_clk	or negedge sys_rst_n)	
+	if(sys_rst_n ==	1'b0)	
+		po_cola <= 1'b0;
+	else 	if((state == TWO) && (pi_money == 1'b1))		
+		po_cola <= 1'b1;
+	else	
+		po_cola <= 1'b0;
+
+property assertion1;
+//    @(posedge sys_clk) (state == TWO) |-> (pi_money == 1'b1);
+    @(posedge sys_clk) (state == IDLE) && (pi_money == 1'b1) |=> (state==ONE);
+endproperty
+
+assert  property(assertion1)
+    $display("assertion worked", $time);
+//else
+//    $display("assertion don't work", $time);
+
+
+
+
+			
+endmodule	
