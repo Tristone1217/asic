5b048ce22f0c64eb059a3b8da9496597c9e0caf7
Tristone1217 <Tristone1217@163.com>
2021-06-10 21:09:31 +0800

    change the readline

--- python/signal_extract/signal_extract.py
+++ python/signal_extract/signal_extract.py
@@ -4,8 +4,10 @@ import re
 pattern = r'module\s+(\w+)'
 pt      = re.compile(pattern)
 test = open('file.txt')
-while True:
-    line = test.readline()
+for line in test.readlines():
     if pt.findall(line):
         signal_name = pt.findall(line)[0]
         print(signal_name)
+
+
+
