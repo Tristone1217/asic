d7ea529d2df900dcf1c89642f287ca8f04a91878
Tristone1217 <Tristone1217@163.com>
2021-06-10 20:39:55 +0800

    up

--- linux/newfile.sv
+++
@@ -1,4 +0,0 @@
-this is a useful example
-this is a useful example for linux command study
-this is a useful example for my linux command study
-useful example useful example  useful example useful example useful example useful example
--- vcs_makefile/file/file_list.f
+++
@@ -1,9 +0,0 @@
--timescale=1ns/1ns
-// Macro define
-//+define+INC_COUNTER
-
-// Source file
-./simple_fsm.v
-
-// Testbench
-./tb_simple_fsm.v
